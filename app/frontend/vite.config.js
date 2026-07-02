45a219880fbf14232759b7289a5e29e5df780ac0
IceJaxi <jaka.marinsek.2002@gmail.com>
2026-07-02 15:39:00 +0200

    Prepare EV Assist for production deployment

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
+  /*   server: {
     port: 5173
-  }
+  } */
+  base: process.env.VITE_BASE_PATH ?? '/ev-assist/',
 });
-
